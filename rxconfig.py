63750f9de4197351c278406c29bea38754b6e9f6
Xiaoguang Pan <201495013@qq.com>
2025-04-22 15:44:33 +0800

    chore: 移除内置的Reflex徽章
    在rxconfig.py中添加了`show_built_with_reflex=False`配置，以移除页面底部的Reflex徽章

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="MAIRE",
+    show_built_with_reflex=False, ## to remove the badge
     tailwind={
         "theme": {
             "extend": {
