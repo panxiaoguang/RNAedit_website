04b266359bffbfec7ea8270a397892bd1fcfbd85
Xiaoguang Pan <201495013@qq.com>
2025-04-22 14:42:22 +0800

    remove confidential info

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="MAIRE",
-    #db_url="postgresql+psycopg://postgres:admin@localhost:5432/reflexdb",
-    db_url="postgresql://postgres.qibqhgitlhnsbmuryfbw:rLh7XyDkrhn058nE@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
     tailwind={
         "theme": {
             "extend": {
@@ -11,5 +9,4 @@ config = rx.Config(
         },
         "plugins": ["@tailwindcss/typography"],
     },
-    backend_port=8010
 )
