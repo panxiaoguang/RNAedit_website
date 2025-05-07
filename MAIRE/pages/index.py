712926e33f61231e1a28fde970d08e53e68544bd
Xiaoguang Pan <201495013@qq.com>
2025-05-07 17:35:34 +0800

    try to exclude db_url in building process

--- MAIRE/pages/index.py
+++ MAIRE/pages/index.py
@@ -1,33 +1,84 @@
 import reflex as rx
 from ..template import template
 from ..styles import info, tooltip
-from ..models import Tissue, RNAeditingtissuelink
-from sqlmodel import select, inspect
-from sqlalchemy import func
+#from ..models import Tissue, RNAeditingtissuelink
+#from sqlmodel import select, inspect
+#from sqlalchemy import func
 from typing import List, Dict
 
 
-def site_numbers_schema(result: tuple) -> List[Dict[str, int]]:
+def site_numbers_schema(result: tuple) -> List[dict]:
     fin_data = []
     for tissue, count in result:
         fin_data.append({"tissue": tissue, "count": count})
     return fin_data
 
+list_site_numbers = [
+    {"tissue": "caudate nucleus", "count": 721799},
+    {"tissue": "inferior frontal gyrus", "count": 681026},
+    {"tissue": "middle frontal gyrus", "count": 742615},
+    {"tissue": "posterior parahippocampal gyrus", "count": 818539},
+    {"tissue": "straight gyrus", "count": 790760},
+    {"tissue": "occipital gyrus", "count": 751928},
+    {"tissue": "anterior cingulate gyrus", "count": 627408},
+    {"tissue": "cerebellum", "count": 940534},
+    {"tissue": "claustrum", "count": 678233},
+    {"tissue": "dentate gyrus", "count": 496510},
+    {"tissue": "globus pallidus", "count": 664163},
+    {"tissue": "inferior occipital gyrus", "count": 713956},
+    {"tissue": "inferior temporal gyrus", "count": 677926},
+    {"tissue": "insular cortex", "count": 748838},
+    {"tissue": "lateral occipitotemporal gyrus", "count": 874697},
+    {"tissue": "middle temporal gyrus", "count": 818346},
+    {"tissue": "pons", "count": 575130},
+    {"tissue": "postcentral gyrus", "count": 751016},
+    {"tissue": "posterior cingulate gyrus", "count": 634583},
+    {"tissue": "precentral gyrus", "count": 995939},
+    {"tissue": "septum", "count": 930400},
+    {"tissue": "superior frontal gyrus", "count": 691665},
+    {"tissue": "superior temporal gyrus", "count": 822372},
+    {"tissue": "supramarginal gyrus", "count": 735064},
+    {"tissue": "angular gyrus", "count": 460603},
+    {"tissue": "preoptic area", "count": 324474},
+    {"tissue": "thalamus", "count": 636520},
+    {"tissue": "annectant gyrus", "count": 555067},
+    {"tissue": "cuneus", "count": 631313},
+    {"tissue": "putamen", "count": 668606},
+    {"tissue": "superior parietal lobule", "count": 875521},
+    {"tissue": "amygdala", "count": 920616},
+    {"tissue": "anterior hypothalamus", "count": 435470},
+    {"tissue": "entorhinal cortex", "count": 673943},
+    {"tissue": "geniculate nucleus", "count": 788372},
+    {"tissue": "orbital gyrus", "count": 763955},
+    {"tissue": "posterior hippocampus", "count": 549647},
+    {"tissue": "posterior hypothalamus", "count": 706760},
+    {"tissue": "spinal cord dorsal", "count": 785565},
+    {"tissue": "subiculum", "count": 820726},
+    {"tissue": "substantia nigra", "count": 566318},
+    {"tissue": "superior colliculus", "count": 321982},
+    {"tissue": "spinal cord ventral", "count": 339521},
+    {"tissue": "anterior hippocampus", "count": 348499},
+    {"tissue": "medulla", "count": 391710},
+    {"tissue": "midbrain", "count": 349261}
+]
+
+
 
 class IndexState(rx.State):
-    @rx.var
-    async def get_numbers_in_tissues(self) -> List[Dict[str, int]]:
-        inspector = inspect(rx.model.get_engine())
-        if not inspector.has_table("tissue"):
-            return []
-        with rx.session() as session:
-            statement = (
-                select(Tissue.name, func.count(RNAeditingtissuelink.rnaediting_id))
-                .join(RNAeditingtissuelink, RNAeditingtissuelink.tissue_id == Tissue.id)
-                .group_by(Tissue.name)
-            )
-            result = session.exec(statement).all()
-            return site_numbers_schema(result)
+    numbers: List[dict] = list_site_numbers
+    #@rx.var
+    #async def get_numbers_in_tissues(self) -> List[Dict[str, int]]:
+    #    inspector = inspect(rx.model.get_engine())
+    #    if not inspector.has_table("tissue"):
+    #        return []
+    #    with rx.session() as session:
+    #        statement = (
+    #            select(Tissue.name, func.count(RNAeditingtissuelink.rnaediting_id))
+    #            .join(RNAeditingtissuelink, RNAeditingtissuelink.tissue_id == Tissue.id)
+    #            .group_by(Tissue.name)
+    #        )
+    #        result = session.exec(statement).all()
+    #        return site_numbers_schema(result)
 
 
 data = [
@@ -174,7 +225,7 @@ def index() -> rx.Component:
                             height=200,
                             interval=0,
                         ),
-                        data=IndexState.get_numbers_in_tissues,
+                        data=IndexState.numbers,
                         width="100%",
                         height=380,
                     ),
