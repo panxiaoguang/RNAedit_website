ded2db81801312cadf142c42ddb82efa4d3ee0a4
Xiaoguang Pan <201495013@qq.com>
2025-06-03 12:55:06 +0800

    remove db url

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="MAIRE",
     show_built_with_reflex=False, ## to remove the badge
-    db_url="postgresql+psycopg2://postgres.qibqhgitlhnsbmuryfbw:CLelSV6CMlh3KI3g@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
     tailwind={
         "theme": {
             "extend": {
